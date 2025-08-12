e46a85113b9bb79a36e3aae5f1b74f2b77ae8088
kelvin-quida <kelvinquida24@gmail.com>
2025-08-12 14:54:46 -0400

    feat: record, save, play audio

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,84 +1,214 @@
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import { Image } from "expo-image";
-import { Platform, StyleSheet, Text } from "react-native";
+import {
+  AudioModule,
+  RecordingPresets,
+  setAudioModeAsync,
+  useAudioPlayer,
+  useAudioRecorder,
+  useAudioRecorderState,
+} from "expo-audio";
+import * as FileSystem from "expo-file-system";
+import * as Sharing from "expo-sharing";
+import React, { useEffect, useState } from "react";
+import {
+  ActionSheetIOS,
+  Alert,
+  Button,
+  FlatList,
+  Platform,
+  Text,
+  View,
+} from "react-native";
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
+export default function App() {
+  const recorder = useAudioRecorder(RecordingPresets.HIGH_QUALITY);
+  const recorderState = useAudioRecorderState(recorder);
+  const player = useAudioPlayer();
+  const [status, setStatus] = useState("pronto");
+
+  const recordingsDir = FileSystem.documentDirectory + "recordings/";
+
+  const [gravacoes, setGravacoes] = useState<string[]>([]);
+
+  useEffect(() => {
+    async function fetchGravacoes() {
+      const lista = await listarGravacoes();
+      setGravacoes(lista);
+    }
+    fetchGravacoes();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const status = await AudioModule.requestRecordingPermissionsAsync();
+      if (!status.granted) {
+        Alert.alert("Permissão não concedida para usar o microfone");
       }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title" className="text-red-500">
-          Welcome!
-        </ThemedText>
-        <Text className="text-red-500">OIisi</Text>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer} className="text-red-600">
-        <ThemedText type="subtitle" className="text-red-600">
-          Step 1: Try it
-        </ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: "cmd + d",
-              android: "cmd + m",
-              web: "F12",
-            })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">
-            npm run reset-project
-          </ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      const dirInfo = await FileSystem.getInfoAsync(recordingsDir);
+      if (!dirInfo.exists) {
+        await FileSystem.makeDirectoryAsync(recordingsDir, {
+          intermediates: true,
+        });
+      }
+      await setAudioModeAsync({
+        playsInSilentMode: true,
+        allowsRecording: true,
+      });
+    })();
+  }, []);
+
+  const handleRecord = async () => {
+    try {
+      setStatus("preparando");
+      await recorder.prepareToRecordAsync();
+      recorder.record();
+      setStatus("gravando");
+    } catch (e) {
+      console.error(e);
+      setStatus("erro");
+    }
+  };
+
+  const handleStop = async () => {
+    const now = new Date();
+
+    const day = now.getDate().toString().padStart(2, "0");
+    const month = (now.getMonth() + 1).toString().padStart(2, "0");
+    const year = now.getFullYear();
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+
+    const dateTimeString = `${day}${month}${year}${hours}${minutes}`;
+
+    try {
+      setStatus("parando");
+      await recorder.stop();
+      const tmpUri = recorder.uri;
+      if (!tmpUri) throw new Error("URI da gravação não encontrada");
+
+      const ext = tmpUri.split(".").pop() ?? "m4a";
+      const newUri = recordingsDir + `${dateTimeString}.${ext}`;
+      await FileSystem.moveAsync({ from: tmpUri, to: newUri });
+      console.log("Gravação salva em:", newUri);
+      setStatus("enviando");
+      setStatus("pronto");
+    } catch (e) {
+      console.error(e);
+      setStatus("erro");
+    }
+  };
+
+  async function listarGravacoes() {
+    try {
+      const arquivos = await FileSystem.readDirectoryAsync(recordingsDir);
+      console.log("Arquivos encontrados:", recordingsDir, arquivos);
+      const caminhosCompletos = arquivos.map((nome) => recordingsDir + nome);
+      console.log("Gravações:", caminhosCompletos);
+      return caminhosCompletos;
+    } catch (err) {
+      console.error("Erro ao listar gravações:", err);
+      return [];
+    }
+  }
+
+  async function playAudio(uri: string) {
+    try {
+      setStatus("tocando");
+      await player.replace(uri);
+      await player.play();
+      setStatus("pronto");
+    } catch (e) {
+      console.error("Erro ao tocar áudio:", e);
+      setStatus("erro");
+    }
+  }
+
+  async function shareAudio(uri: string) {
+    const disponivel = await Sharing.isAvailableAsync();
+    if (!disponivel) {
+      alert("Compartilhamento não disponível neste dispositivo");
+      return;
+    }
+    await Sharing.shareAsync(uri);
+  }
+
+  async function escolherECompartilharGravacao() {
+    const arquivos = await listarGravacoes();
+
+    if (!arquivos.length) {
+      Alert.alert("Nenhuma gravação encontrada");
+      return;
+    }
+
+    if (Platform.OS === "ios") {
+      ActionSheetIOS.showActionSheetWithOptions(
+        {
+          options: [
+            ...arquivos.map((a) => a.split("/").pop() ?? ""),
+            "Cancelar",
+          ],
+          cancelButtonIndex: arquivos.length,
+          title: "Escolha uma gravação para compartilhar",
+        },
+        async (buttonIndex) => {
+          if (buttonIndex < arquivos.length) {
+            await shareAudio(arquivos[buttonIndex]);
+          }
+        }
+      );
+    } else {
+      Alert.alert(
+        "Escolha uma gravação",
+        "",
+        [
+          ...arquivos.map(
+            (uri) => (
+              console.log("Compartilhando:", uri),
+              {
+                text: uri.split("/").pop()!,
+                onPress: async () => await shareAudio(uri),
+              }
+            )
+          ),
+          { text: "Cancelar", style: "cancel" },
+        ],
+        { cancelable: true }
+      );
+    }
+  }
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", padding: 20 }}>
+      <Text>Status: {status}</Text>
+      <Button
+        title={
+          recorderState.isRecording ? "Parar Gravação" : "Iniciar Gravação"
+        }
+        onPress={recorderState.isRecording ? handleStop : handleRecord}
+      />
+      <Button
+        title="Listar Gravações"
+        onPress={async () => {
+          const arquivos = await listarGravacoes();
+          Alert.alert("Gravações", arquivos.join("\n"));
+        }}
+      />
+      <Button
+        title="Compartilhar Gravação"
+        onPress={escolherECompartilharGravacao}
+      />
+      <FlatList
+        data={gravacoes}
+        keyExtractor={(item: any) => item}
+        renderItem={({ item }: { item: string }) => (
+          <View>
+            <Text>{item}</Text>
+            <Button
+              title={`${item.split("/").pop()!}`}
+              onPress={() => playAudio(item)}
+            />
+          </View>
+        )}
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-});
